c0c341fb09adbe6643ce40049ef46ae3d2416671
tawfik elmetwally <110994276+tawfikhammad@users.noreply.github.com>
2024-05-17 23:12:04 +0300

    Update dim_product.sql

--- Design Data Mart/dim_product.sql
+++ Design Data Mart/dim_product.sql
@@ -3,16 +3,13 @@ USE EO_AdventureWorksDW2019
 go
 
 -- dropping the foreign keys
-IF EXISTS (SELECT *
-           FROM   sys.foreign_keys
-           WHERE  NAME = 'fk_fact_sales_dim_product' AND parent_object_id = Object_id('fact_sales'))
-  ALTER TABLE fact_sales
+IF EXISTS (SELECT * FROM   sys.foreign_keys WHERE  NAME = 'fk_fact_sales_dim_product' AND parent_object_id = Object_id('fact_sales'))
+
+ALTER TABLE fact_sales
     DROP CONSTRAINT fk_fact_sales_dim_product;
 
 -- Drop and create the table
-IF EXISTS (SELECT *
-           FROM   sys.objects
-           WHERE  NAME = 'dim_product' AND type = 'U')
+IF EXISTS (SELECT * FROM   sys.objects WHERE  NAME = 'dim_product' AND type = 'U')
   DROP TABLE dim_product
 
 go
@@ -38,59 +35,22 @@ CREATE TABLE dim_product
      CONSTRAINT pk_dim_product PRIMARY KEY CLUSTERED (product_key)
   );
 
--- Insert unknown record
-SET IDENTITY_INSERT dim_product ON
-
-INSERT INTO dim_product
-            (product_key,
-             product_id,
-             product_name,
-             Product_description,
-             product_subcategory,
-             product_category,
-             color,
-             model_name,
-             reorder_point,
-             standard_cost,
-             source_system_code,
-             start_date,
-             end_date,
-             is_current)
-VALUES      (0,
-             0,
-             'Unknown',
-             'Unknown',
-             'Unknown',
-             'Unknown',
-             'Unknown',
-             'Unknown',
-             0,
-             0,
-             0,
-             '1900-01-01',
-             NULL,
-             1)
-
-SET IDENTITY_INSERT dim_product OFF
 
 -- create foreign key
-IF EXISTS (SELECT *
-           FROM   sys.tables
-           WHERE  NAME = 'fact_sales')
-  ALTER TABLE fact_sales
+IF EXISTS (SELECT * FROM   sys.tables WHERE  NAME = 'fact_sales')
+
+ALTER TABLE fact_sales
     ADD CONSTRAINT fk_fact_sales_dim_product FOREIGN KEY (product_key) REFERENCES dim_product(product_key);
 
 -- create indexes
-IF EXISTS (SELECT *
-           FROM   sys.indexes
-           WHERE  NAME = 'dim_product_product_id' AND object_id = Object_id('dim_product'))
+IF EXISTS (SELECT * FROM   sys.indexes WHERE  NAME = 'dim_product_product_id' AND object_id = Object_id('dim_product'))
   DROP INDEX dim_product.dim_product_product_id;
 
 CREATE INDEX dim_product_product_id ON dim_product(product_id);
 
-IF EXISTS (SELECT *
-           FROM   sys.indexes
-           WHERE  NAME = 'dim_prodct_product_category' AND object_id = Object_id('dim_product'))
+--
+
+IF EXISTS (SELECT * FROM   sys.indexes WHERE  NAME = 'dim_prodct_product_category' AND object_id = Object_id('dim_product'))
   DROP INDEX dim_product.dim_prodct_product_category
 
 CREATE INDEX dim_prodct_product_category ON dim_product(product_category); 
